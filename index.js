18c989bb71a8548f83ff3f87d63879ca69162f13
dolymood <dolymood@gmail.com>
2020-09-17 14:46:32 +0800

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+var nativeGetOwnPropertyNames = Object.getOwnPropertyNames
+
+function getOwnPropertyNames (o) {
+  var result = nativeGetOwnPropertyNames(o)
+  if (typeof o === 'function') {
+    var IEKeys = ['caller', 'arguments']
+    // fix IE Object.getOwnPropertyNames returns caller and arguments in strict mode for function instance
+    // https://github.com/zloirock/core-js/issues/825
+    result = result.filter(function (k) {
+      return IEKeys.indexOf(k) === -1
+    })
+  }
+  return result
+}
+
+getOwnPropertyNames.toString = function () {
+  return 'function getOwnPropertyNames() { [native code] }'
+}
+
+Object.defineProperty(Object, 'getOwnPropertyNames', {
+  configurable: true,
+  enumerable: false,
+  writable: true,
+  value: getOwnPropertyNames
+})
